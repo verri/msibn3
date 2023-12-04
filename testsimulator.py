d2082db0c061d2e23a669b9f302704eeaf5425b4
Filipe Verri <filipeverri@gmail.com>
2023-12-04 18:18:21 -0300

    Fixes in test simulator.

--- testsimulator.py
+++ testsimulator.py
@@ -1,6 +1,9 @@
 from datasets.hdf5 import FlightSimulator
+import numpy as np
+import h5py
 
-simulator = FlightSimulator("pqtec-20231128.hdf5")
+file = h5py.File("pqtec-20231128.hdf5", "r")
+simulator = FlightSimulator(file)
 
 rng = np.random.default_rng()
 for segment in simulator.generate(rng):
