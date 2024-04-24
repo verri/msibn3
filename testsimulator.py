93bf65911fa85d07ea88586d7e87987d77b73fa6
Filipe Verri <filipeverri@gmail.com>
2024-04-24 10:24:46 -0300

    More complete test.

--- testsimulator.py
+++ testsimulator.py
@@ -1,10 +1,20 @@
-from datasets.hdf5 import FlightSimulator, polar2cartesian
+from datasets.hdf5 import FlightSimulator, polar2cartesian, Frame
 import numpy as np
 import h5py
 
-file = h5py.File("pqtec-20231128.hdf5", "r")
-simulator = FlightSimulator(file)
+import argparse
+
+parser = argparse.ArgumentParser(description="Simulate a flight path")
+parser.add_argument("filename", type=str, help="The filename of the flight path")
+args = parser.parse_args()
+
+file = h5py.File(args.filename, "r")
+simulator = FlightSimulator([file])
+
+def info(frame: Frame):
+    return f"(time={frame.time}, yaw={frame.yaw}, altitude={frame.altitude})"
 
 rng = np.random.default_rng()
 for segment in simulator.generate(rng):
-    print(*polar2cartesian((segment.distance, segment.bearing)))
+    print(*[info(frame) for frame in segment.frames], segment.distance,
+            segment.bearing, *polar2cartesian((segment.distance, segment.bearing)))
