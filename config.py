5135204d2adfea0528ef5c3564d0594de61c7743
Filipe Verri <filipeverri@gmail.com>
2024-04-24 10:28:47 -0300

    Small fix.

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 
-Params = namedtuple('Params', ['INPUT_SHAPE', 'BATCH_SIZE', 'EPOCHS', 'MAX_ALTITUDE'])
+Params = namedtuple('Params', ['INPUT_SHAPE', 'BATCH_SIZE', 'MAX_ALTITUDE',
+    'STEPS_PER_EPOCH', 'EPOCHS'])
 
 params = Params(
     INPUT_SHAPE = (90, 160, 8), # rows x cols (instead of width x height)
